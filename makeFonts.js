d4f57c18decbaa6687c52951fd92573a4953d219
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-11-04 21:19:19 +0900

    Refactor makeFonts.js

--- makeFonts.js
+++ makeFonts.js
@@ -4,11 +4,8 @@ const fs = require('fs');
 const path = './fonts';
 fs.readdir(path, (err, files) => {
     let fontList = [];
-    let length = files.length - 1;
-    files.forEach((file, index) => {
+    for (let file of files) {
         fontList.push(`jsPDFAPI.addFileToVFS('${file}','${new Buffer(fs.readFileSync(path + '/' + file)).toString('base64')}');`);
-        if (length === index) {
-            fs.writeFileSync('./dist/default_vfs.js', `(function (jsPDFAPI) { \n"use strict";\n${fontList.join('\n')}\n})(jsPDF.API);`);
-        }
-    });
+    }
+    fs.writeFileSync('./dist/default_vfs.js', `(function (jsPDFAPI) { \n"use strict";\n${fontList.join('\n')}\n})(jsPDF.API);`);
 })
